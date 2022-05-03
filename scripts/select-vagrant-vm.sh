761532c100dffc7e92edc92ee3b36315424186b0
Wittano <radoslaw.ratyna@gmail.com>
2022-05-03 11:03:01 +0200

    feat(script-vagrant): add new flag: start
    Flag "start": run vm and switch to vm by ssh connection

--- scripts/select-vagrant-vm.sh
+++ scripts/select-vagrant-vm.sh
@@ -2,27 +2,35 @@
 set -eo pipefail
 
 _error_msg() {
-    echo $1
-    exit -1
+    echo "$1"
+    exit 1
 }
 
-if [ -z $1 ] || [ -z $2 ]; then
-    _error_msg 'Invalid arguments: vm <vm_name> <vagrant_option>' 
-fi 
+_destroy_vm() {
+    vagrant destroy -f
+}
 
-if [ -z $(command -v vagrant) ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
+    _error_msg 'Invalid arguments: vm <vm_name> <vagrant_option>'
+fi
+
+if [ -z "$(command -v vagrant)" ]; then
     _error_msg 'Vagrant not found!'
 fi
 
 vms_dir=$HOME/projects/config/system/vms
-vms=$(ls $vms_dir)
+vms=$(ls "$vms_dir")
 
 if [ "$1" == "all" ]; then
     for dir in $vms; do
         echo "$dir - virutal machine"
-        cd $vms_dir/$dir
+        cd "$vms_dir/$dir"
 
-        vagrant $2
+        if [ "$2" != "destroy" ]; then
+            vagrant "$2" -f
+        else
+            vagrant "$2"
+        fi
     done
 
     exit 0
@@ -32,15 +40,22 @@ for dir in $vms; do
     if [ "$dir" == "$1" ]; then
         vm_dir=$vms_dir/$dir
 
-        cd $vm_dir
+        cd "$vm_dir"
 
-        mkdir -p $vm_dir/data
+        mkdir -p "$vm_dir"/data
 
-        if [ $2 == 'rebuild' ]; then
-            vagrant destroy
+        case $2 in
+        'rebuild')
+            _destroy_vm
             vagrant up
-        else
-            vagrant $2
-        fi
+            ;;
+        'start')
+            vagrant up
+            vagrant ssh
+            ;;
+        *)
+            vagrant "$2"
+            ;;
+        esac
     fi
 done
