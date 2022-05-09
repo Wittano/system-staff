9e6e47a49eda014bf80adb5ea928228011756c87
Wittano <radoslaw.ratyna@gmail.com>
2022-05-09 22:33:02 +0200

    chore(script): fix warning in wacom script

--- scripts/wacom-multi-monitor.sh
+++ scripts/wacom-multi-monitor.sh
@@ -1,12 +1,13 @@
 #!/usr/share/env bash
+set -eo pipefail
 
-if [ -z `which xsetwacom` ]; then
+if [ -z "$(which xsetwacom)" ]; then
     echo "Wacom driver isn't installed!"
-    exit -1;
+    exit 1
 fi
 
-tablet=`xsetwacom list devices | awk '{print $9}'`
+TABLET=$(xsetwacom list devices | awk '{print $9}')
 
-for i in $tablet; do
+for i in $TABLET; do
     xsetwacom --set "${i}" MapToOutput HEAD-0
 done
