132e2bcd6e1c773a1d63bcb1a894e3b92a084439
Wittano <radoslaw.ratyna@gmail.com>
2022-05-18 23:26:48 +0200

    fix(backup-script): removed dangerous practice

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -2,16 +2,16 @@
 
 if [ -z "$(command -v rsync)" ]; then
     echo "rsync not found";
-    exit -1
+    exit 1
 fi
 
 BACKUP_DIR=/mnt/backup
 
 if [ ! -d $BACKUP_DIR ]; then
     sudo mkdir -p $BACKUP_DIR
-    sudo chown $(whoami) $BACKUP_DIR
+    sudo chown "$(whoami)" $BACKUP_DIR
 fi
 
-CURRENT_DIR=$(dirname $0)
+CURRENT_DIR=$(dirname "$0")
 
-rsync -aAXv --delete --exclude-from="${CURRENT_DIR}/exclude.txt" $HOME /mnt/backup
+rsync -aAXv --delete --exclude-from="${CURRENT_DIR}/exclude.txt" "$HOME" /mnt/backup
