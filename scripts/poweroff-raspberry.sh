4930cca5cd6d94adb7a26514a73ea632bf0848e6
Wittano <radoslaw.ratyna@gmail.com>
2022-05-21 15:30:30 +0200

    fix(shutdown-raspberry): invalid path in service

--- scripts/poweroff-raspberry.sh
+++ scripts/poweroff-raspberry.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-IP="192.168.1.8"
+RASPBERRY_IP="192.168.1.8"
 
-ping -c 1 -w 1 $IP > /dev/null
-
-if [ $? -eq 0 ]; then
-    ssh -i $HOME/.ssh/id_rsa wittano@$IP "sudo poweroff"
+if ! ping -c 1 -w 1 $RASPBERRY_IP >/dev/null; then
+    ssh -i "$HOME/.ssh/id_rsa wittano@$RASPBERRY_IP" "sudo poweroff"
 fi
