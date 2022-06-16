82b2f0a372a9594ad26d4d108770dadf0ceb3e56
Wittano <radoslaw.ratyna@gmail.com>
2022-06-16 12:00:26 +0200

    feat(terraform): removed terraform configuration

--- terraform/raspberry/network.tf
+++
@@ -1,8 +0,0 @@
-resource "docker_network" "gateway_network" {
-  name = "traefik-network"
-  internal = true
-}
-
-resource "docker_network" "public_gateway_network" {
-  name = "gateway-network"
-}
